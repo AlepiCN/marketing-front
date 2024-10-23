ed399acb246374ac5e6ce4acfe5b3caaa4cac56a
fanleichao <351717461@qq.com>
2024-10-23 11:57:12 +0800

    feat:初始化前端项目

---
+++ src/api/index.tsx
@@ -0,0 +1,40 @@
+// 请求地址
+const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "https://console-mock.apipost.cn/mock/6afa907d-6678-45e2-b867-032a11090abd";
+
+/**
+ * 查询抽奖奖品列表
+ * @param strategyId 策略ID
+ */
+export const queryRaffleAwardList = (strategyId: number) => {
+    try {
+        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list?apipost_id=1bdf4d84781001&strategyId=${strategyId}`, {
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        });
+    } catch (error) {
+        return fetch("{\n" +
+            "    \"code\": \"0001\",\n" +
+            "    \"info\": \"调用失败\",\n" +
+            "    \"data\": [\n" +
+            "}");
+    }
+}
+
+export const randomRaffle = (strategyId: number) => {
+    try {
+        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle?apipost_id=1bdf4d84781001&strategyId=${strategyId}`, {
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        })
+    } catch (error) {
+        return fetch("{\n" +
+            "    \"code\": \"0001\",\n" +
+            "    \"info\": \"调用失败\",\n" +
+            "    \"data\": [\n" +
+            "}");
+    }
+}
