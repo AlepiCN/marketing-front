a306b48880c64fb0e34d4795a04325c477ad78b7
fanleichao <351717461@qq.com>
2024-10-24 15:43:37 +0800

    feat:对接后端接口

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -5,13 +5,16 @@ const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "https:
  * 查询抽奖奖品列表
  * @param strategyId 策略ID
  */
-export const queryRaffleAwardList = (strategyId: number) => {
+export const queryRaffleAwardList = (strategyId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list?apipost_id=1bdf4d84781001&strategyId=${strategyId}`, {
-            method: 'get',
+        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list`, {
+            method: 'POST',
             headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                strategyId: strategyId
+            })
         });
     } catch (error) {
         return fetch("{\n" +
@@ -22,13 +25,30 @@ export const queryRaffleAwardList = (strategyId: number) => {
     }
 }
 
-export const randomRaffle = (strategyId: number) => {
+/**
+ * 随机抽奖接口
+ * @param strategyId 策略ID
+ *
+ * {
+ * 	"code": "0000",
+ * 	"info": "调用成功",
+ * 	"data": {
+ * 	    "awardIndex": 1, // awardIndex 获得的是列表中第几个奖品，方便测试使用
+ * 		"awardId": 535,
+ * 		"awardTitle": "一部手机"
+ * 	}
+ * }
+ */
+export const randomRaffle = (strategyId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle?apipost_id=1bdf4d84781001&strategyId=${strategyId}`, {
-            method: 'get',
+        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
-            }
+            },
+            body: JSON.stringify({
+                strategyId: strategyId
+            })
         })
     } catch (error) {
         return fetch("{\n" +
@@ -37,4 +57,17 @@ export const randomRaffle = (strategyId: number) => {
             "    \"data\": [\n" +
             "}");
     }
+}
+
+/**
+ * 装配抽奖
+ * @param strategyId
+ */
+export const strategyArmory = (strategyId?: number) => {
+    return fetch(`${apiHostUrl}/api/v1/raffle/strategy_armory?strategyId=${strategyId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 }
--- src/app/pages/lucky/lucky-wheel-page.tsx
+++ src/app/pages/lucky/lucky-wheel-page.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import React, {useEffect, useState, useRef} from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 // @ts-ignore
-import {LuckyWheel} from '@lucky-canvas/react'
+import { LuckyWheel } from '@lucky-canvas/react'
 
-import {queryRaffleAwardList, randomRaffle} from '@/api'
-import {RaffleAwardVO} from '@/types/RaffleAwardVO'
+import { queryRaffleAwardList, randomRaffle } from '@/api'
+import { RaffleAwardVO } from '@/types/RaffleAwardVO'
 
 
 export function LuckyWheelPage() {
@@ -13,16 +13,16 @@ export function LuckyWheelPage() {
     let queryParams;
 
     if (typeof window !== 'undefined') {
-    queryParams = new URLSearchParams(window.location.search);
+        queryParams = new URLSearchParams(window.location.search);
     } else {
-    // 处理非浏览器环境的情况
-    queryParams = new URLSearchParams();
+        // 处理非浏览器环境的情况
+        queryParams = new URLSearchParams();
     }
     const strategyId = Number(queryParams.get('strategyId'));
     const [prizes, setPrizes] = useState([{}]);
 
     const [blocks] = useState([
-        {padding: '10px', background: '#869cfa', imgs: [{src: "https://bugstack.cn/images/system/blog-03.png"}]}
+        { padding: '10px', background: '#869cfa', imgs: [{ src: "https://bugstack.cn/images/system/blog-03.png" }] }
     ])
     // const [prizes] = useState([
     //     {background: '#e9e8fe', fonts: [{text: '0'}]},
@@ -34,19 +34,19 @@ export function LuckyWheelPage() {
     // ])
 
     const [buttons] = useState([
-        {radius: '40%', background: '#617df2'},
-        {radius: '35%', background: '#afc8ff'},
+        { radius: '40%', background: '#617df2' },
+        { radius: '35%', background: '#afc8ff' },
         {
             radius: '30%', background: '#869cfa',
             pointer: true,
-            fonts: [{text: '开始', top: '-10px'}]
+            fonts: [{ text: '开始', top: '-10px' }]
         }
     ])
     const myLucky = useRef()
 
     const queryRaffleAwardListHandle = async () => {
         const result = await queryRaffleAwardList(strategyId);
-        const {code, info, data} = await result.json();
+        const { code, info, data } = await result.json();
         if (code != "0000") {
             window.alert("获取抽奖奖品列表失败 code:" + code + " info:" + info)
             return;
@@ -54,9 +54,10 @@ export function LuckyWheelPage() {
         // 创建一个新的奖品数组
         const prizes = data.map((award: RaffleAwardVO, index: number) => {
             const background = index % 2 === 0 ? '#e9e8fe' : '#b8c5f2';
+            console.log(index + ":" + award.awardId);
             return {
                 background: background,
-                fonts: [{id: award.awardId, text: award.awardTitle, top: '15px'}]
+                fonts: [{ id: award.awardId, text: award.awardTitle, top: '15px' }]
             };
         });
         // 设置奖品数据
@@ -66,16 +67,16 @@ export function LuckyWheelPage() {
     // 调用随机抽奖
     const randomRaffleHandle = async () => {
         const result = await randomRaffle(strategyId);
-        const {code, info, data} = await result.json();
+        const { code, info, data } = await result.json();
         if (code != "0000") {
             window.alert("获取抽奖奖品列表失败 code:" + code + " info:" + info)
             return;
         }
         // 为了方便测试，mock 的接口直接返回 awardIndex 也就是奖品列表中第几个奖品。
-        return data.awardIndex ? data.awardIndex : prizes.findIndex(prize =>
+        return data.awardIndex ? data.awardIndex - 1 : prizes.findIndex(prize =>
             //@ts-ignore
             prize.fonts.some(font => font.id === data.awardId)
-        ) + 1;
+        );
     }
 
     useEffect(() => {
@@ -97,9 +98,9 @@ export function LuckyWheelPage() {
                 setTimeout(() => {
                     // 抽奖接口
                     randomRaffleHandle().then(prizeIndex => {
-                            // @ts-ignore
-                            myLucky.current.stop(prizeIndex);
-                        }
+                        // @ts-ignore
+                        myLucky.current.stop(prizeIndex);
+                    }
                     );
                 }, 2500)
             }}
